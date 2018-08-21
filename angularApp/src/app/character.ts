974e66823ab750b02aeef7007c06019d7a22cecc
amalfushi <amalfushi@gmail.com>
2018-08-20 23:05:07 -0700

    Added optional character details and started dice roll input

--- angularApp/src/app/character.ts
+++ angularApp/src/app/character.ts
@@ -3,6 +3,10 @@ export class Character {
     name: string = "";
     player_name: string = "";
     initiative: number;
+    health: number;
+    defense: number;
+    new_roll: string = "3d6";
+    rolls: string[] = [];
 
     constructor(values: Object = {}) {
         Object.assign(this, values);
