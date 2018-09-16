7ae91ba1e1c6d4378136cb4c9847cf7f4ff5d737
amalfushi <amalfushi@gmail.com>
2018-09-15 22:57:20 -0700

    testing components

--- angularApp/src/app/character-card/character-card.component.spec.ts
+++ angularApp/src/app/character-card/character-card.component.spec.ts
@@ -1,36 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharacterCardComponent } from './character-card.component';
+import { FormsModule } from '@angular/forms';
 import { CharacterDataService } from '../character-data.service';
+import { DiceService } from '../dice.service';
 import { Character } from '../character';
 
 describe('CharacterCardComponent', () => {
   let component: CharacterCardComponent;
   let fixture: ComponentFixture<CharacterCardComponent>;
+  let testCharacter: Character;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CharacterCardComponent ],
-      providers : [CharacterDataService ],
+      declarations: [CharacterCardComponent],
+      imports: [FormsModule],
+      providers: [CharacterDataService, DiceService],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    let testCharacter = new Character();
-    testCharacter.id = 1;
-    testCharacter.initiative = 12;
-    testCharacter.name = "TEST CHARACTER";
-    testCharacter.player_name = "TEST PLAYER";
-    
     fixture = TestBed.createComponent(CharacterCardComponent);
     component = fixture.componentInstance;
-    component.character = testCharacter;
-
     fixture.detectChanges();
+    
+    testCharacter = new Character();
+    testCharacter.name = "TEST CHARACTER";
+    // });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should display character name', () => {
+  //   component.character = testCharacter;
+  //   expect(component.character.name).toBe('TEST CHARACTER')
+  // });
 });
--- angularApp/src/app/character-list/character-list.component.spec.ts
+++ angularApp/src/app/character-list/character-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharacterListComponent } from './character-list.component';
+
 import { FormsModule } from '@angular/forms';
 import { DndModule } from 'ng2-dnd';
 import { CharacterCardComponent } from '../character-card/character-card.component';
@@ -9,44 +10,50 @@ import { Character } from '../character';
 describe('Character List Component', () => {
   let component: CharacterListComponent;
   let fixture: ComponentFixture<CharacterListComponent>;
+  let testCharacter: Character;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CharacterListComponent,
-      CharacterCardComponent ],
+      declarations: [CharacterListComponent,
+        CharacterCardComponent],
       imports: [FormsModule, DndModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CharacterListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    testCharacter = new Character();
+    testCharacter.name = "TEST CHARACTER";
+    testCharacter.player_name = "TEST PLAYER";
+    testCharacter.initiative = 12;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have empty character list', ()=> {
+  it('should have empty character list', () => {
     expect(component.characters.length).toBe(0)
   })
 
-  it('.addCharacter should do nothing', ()=> {
+  it('.addCharacter should do nothing', () => {
     component.addCharacter();
     expect(component.characters.length).toBe(0);
   })
 
-  it('.addCharacter should add TEST CHARACTER', ()=> {
-    let testCharacter = new Character();
-    testCharacter.name = "TEST CHARACTER";
-    testCharacter.player_name = "TEST PLAYER";
-    testCharacter.initiative = 12;
-
+  it('.addCharacter should add TEST CHARACTER', () => {
     component.newCharacter = testCharacter;
     component.addCharacter();
+    expect(component.characters.length).toBe(1);
     expect(component.characters[0]).toBe(testCharacter);
-  })
+  });
 
+  it('.delteCharacter should remove TEST CHARACTER', () => {
+    component.removeCharacter(testCharacter);
+    expect(component.characters.length).toBe(0);
+  });
 });
