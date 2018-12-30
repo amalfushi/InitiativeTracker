cca99b8680caf2a7de28083479215e287a3ba490
amalfushi <amalfushi@gmail.com>
2018-12-29 23:37:34 -0800

    Updated port for azure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express"),
     app = express(),
     bp = require("body-parser"),
     path = require("path"),
-    port = 8000;
+    port = 80;
 
 app.use(express.static(path.join(__dirname, "./public/dist")));
 app.use(bp.json());
