f7ed75f1feb318b251934e5ceaf5204e45b4d390
amalfushi <amalfushi@gmail.com>
2019-01-02 23:12:45 -0800

    Safety Route Fix

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,6 +2,6 @@ const path = require("path");
 
 module.exports = function (app) {
     app.all("*", (req, res, next) => {
-        res.sendFile(path.resolve("./angularApp/dist/index.html"));
+        res.sendFile(path.resolve("./public/dist/index.html"));
     });
 }
